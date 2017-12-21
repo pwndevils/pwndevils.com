5aea7e6424097cbb8aea26bd78827e5c4615d648
Wil Gibbs <wfgibbs@asu.edu>
2017-12-20 22:55:58 -0700

    Wierd issue where the dropdown wasn't animating if it was below the .read()

--- js/main.js
+++ js/main.js
@@ -1,15 +1,14 @@
-$document.ready(function(){
-            $('[data-spy="scroll"]').each(function () {
-                var $spy = $(this).scrollspy('refresh')
-                }); 
-        });
-
 $("#video-down").click(function() {
-	$("#toggle-section").slideToggle(225);
-	img = document.getElementById("img-down");
-	if (img.src.indexOf("down") != -1) {
-		img.src = "img/up.png";
-	} else {
-		img.src = "img/down.png";
-	}
+    $("#toggle-section").slideToggle(225);
+    img = document.getElementById("img-down");
+    if (img.src.indexOf("down") != -1) {
+        img.src = "img/up.png";
+    } else {
+        img.src = "img/down.png";
+    }
+});
+$document.ready(function() {
+    $('[data-spy="scroll"]').each(function() {
+        var $spy = $(this).scrollspy('refresh')
+    });
 });

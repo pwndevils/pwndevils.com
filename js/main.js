9abb464793ecc12c22b3b6684f42461b7ce8dc38
Wil Gibbs <wfgibbs@asu.edu>
2017-12-21 00:56:31 -0700

    Added a nice loading feater to wait for the banner image to load, hopefully not too long a wait

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 $("#video-down").click(function() {
     $("#toggle-section").slideToggle(225);
+
+
     img = document.getElementById("img-down");
     if (img.src.indexOf("down") != -1) {
         img.src = "img/up.png";
@@ -7,8 +9,51 @@ $("#video-down").click(function() {
         img.src = "img/down.png";
     }
 });
-$document.ready(function() {
-    $('[data-spy="scroll"]').each(function() {
-        var $spy = $(this).scrollspy('refresh')
-    });
+
+$("#banner-img")
+    .on('load', function() { handler(); })
+    .on('error', function() { console.log("error loading image"); })
+    .attr("src", $("#banner-img").attr("src"))
+;
+
+var banner = $("#banner-img");
+if (banner.complete) {
+    // Already loaded, call the handler directly
+	console.log("Calling Loader");
+    handler();
+}
+else {
+	console.log("Added to loader");
+    // Not loaded yet, register the handler
+    banner.onload = handler;
+}
+function handler() {
+	$('body').toggleClass('loaded');
+	console.log("called");
+}
+
+$(".nav li a").on('click', function(event) {
+
+    console.log("Before Hash");
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+        console.log("After Hash");
+
+        // Prevent default anchor click behavior
+        event.preventDefault();
+
+        // Store hash
+        var hash = this.hash;
+
+        // Using jQuery's animate() method to add smooth page scroll
+        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+        $('html, body').animate({
+            scrollTop: $(hash).offset().top - (window.innerHeight / 20)
+        }, 400, function() {
+        	console.log("SCROLLING");
+            // Add hash (#) to URL when done scrolling (default click behavior)
+            //window.location.hash = hash;
+        });
+    }
 });
+

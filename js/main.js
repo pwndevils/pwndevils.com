52e609cb6249ee26983f96364314119eb15a9b8c
Wil Gibbs <wfgibbs@asu.edu>
2017-12-20 21:44:54 -0700

    Added join mailing list/slack buttons. Added twitter follow button.  Changed some text to be based on em so it scales correctly for mobile.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
 $("#video-down").click(function() {
-	$("#toggle-section").slideToggle();
+	$("#toggle-section").slideToggle(225);
+	img = document.getElementById("img-down");
+	if (img.src.indexOf("down") != -1) {
+		img.src = "img/up.png";
+	} else {
+		img.src = "img/down.png";
+	}
 });

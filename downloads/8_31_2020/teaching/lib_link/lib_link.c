e2afed6691fb31d70ea16cf051b2667411e25256
Pascal <nksmith6@asu.edu>
2020-08-31 16:12:22 -0700

    uplaoded challenge resources

---
+++ downloads/8_31_2020/teaching/lib_link/lib_link.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() 
+{
+    int value = 15;
+    printf("This is the value: %d\n",value);
+    return value; 
+}
---
+++ downloads/8_31_2020/teaching/stack_dec/stack_dec.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() 
+{
+    int value = 15;
+    return value; 
+}
